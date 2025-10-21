ccb99d9b5d1b41eb41a7e879358b324ce7b94baa
Md. Mohebullah <mohebullah.cse@gmail.com>
2025-10-21 15:36:31 +0600

    fix the build warning issue from this file

--- Sector-13-wllfare-socity-project/Models/SmtpSettings.cs
+++ Sector-13-wllfare-socity-project/Models/SmtpSettings.cs
@@ -2,10 +2,10 @@ namespace Sector_13_Welfare_Society___Digital_Management_System.Models
 {
     public class SmtpSettingsModel
     {
-        public string Host { get; set; }
+        public string Host { get; set; } = string.Empty;
         public int Port { get; set; }
         public bool EnableSSL { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
